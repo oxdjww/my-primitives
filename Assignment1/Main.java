bf282d2b30d3c6be782b6f1fe2923f970737fe96
oxdjww <oxdjww@gmail.com>
2023-03-30 00:16:21 +0900

    src upload

---
+++ Assignment1/Main.java
@@ -0,0 +1,67 @@
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Security.addProvider(new BouncyCastleProvider());
+        MessageDigest SHA1 = MessageDigest.getInstance("SHA-1");
+        //내 학번
+        String stuNum = "20192944";
+        //학번 byte[]
+        byte[] stuNumBytes = Utils.toByteArray(stuNum);
+        //난수
+        int randomInt = 0;
+        //난수 byte[]
+        byte[] randomBytes = new byte[16];
+
+        //조건에 만족하는 난수를 찾을 때까지
+        while(true){
+            Random random = new Random();
+            //난수 생성
+            randomInt = random.nextInt();
+            //학번 + 난수
+            String combinedString = stuNum + randomInt;
+            //combinedString byte[] 변환
+            byte[] combinedBytes = Utils.toByteArray(combinedString);
+            //update & digest
+            SHA1.update(combinedBytes);
+            byte[] myHash = SHA1.digest();
+
+
+            if (checkCondition1(myHash)) {
+                System.out.println("조건 1 : 해시값의 첫 번째 바이트가 0이 되게 하는 난수 찾기");
+                System.out.println("myRandomNumber : " + randomInt);
+                System.out.println("length : " + myHash.length);
+                System.out.print("Hash bytes : ");
+                for(byte b : myHash)
+                    System.out.print(b + " ");
+                System.out.println();
+                System.out.println("hash hex : " + Utils.toHexString(myHash));
+                break;
+            }
+//            if (checkCondition2(myHash)) {
+//                System.out.println("조건 2 : 해시값의 첫 번째, 두 번째 바이트가 0이 되게 하는 난수 찾기");
+//                System.out.println("myRandomNumber : " + randomInt);
+//                System.out.println("length : " + myHash.length);
+//                System.out.print("Hash bytes : ");
+//                for(byte b : myHash)
+//                    System.out.print(b + " ");
+//                System.out.println();
+//                System.out.println("hash hex : " + Utils.toHexString(myHash));
+//                break;
+//            }
+        }
+    }
+    private static boolean checkCondition1(byte[] hash) {
+        //첫 byte = 0
+        return hash[0] == 0 ;
+    }
+    private static boolean checkCondition2(byte[] hash) {
+        //첫 byte, 두번째 byte = 0
+        return hash[0] == 0 && hash[1] == 0 ;
+    }
+
+}
---
+++ Assignment1/Utils.java
@@ -0,0 +1,32 @@
+
+public class Utils {
+	public static String toString(byte[] input) {
+		return new String(input);
+	}
+	public static String toHexString(byte[] input) {
+		String hexString = "";
+		int length = input.length;
+
+		for (int i = 0; i < length; i++) {
+			if (i != length - 1) {
+				hexString += String.format("%02X:", input[i]);
+			} else {
+				hexString += String.format("%02X", input[i]);
+			}
+		}
+
+		return hexString;
+	}
+
+
+	public static byte[] toByteArray(String string) {
+		byte[] bytes = new byte[string.length()];
+		char[] chars = string.toCharArray();
+
+		for (int i = 0; i != chars.length; i++) {
+			bytes[i] = (byte) chars[i];
+		}
+
+		return bytes;
+	}
+}
